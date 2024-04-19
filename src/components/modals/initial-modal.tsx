2a921fbc84f01bd22af6c8812822ecd7e887af16
AA <talentyouth99@gmail.com>
2024-04-19 22:30:38 +0800

    UploadThing setup

--- src/components/modals/initial-modal.tsx
+++ src/components/modals/initial-modal.tsx
@@ -2,13 +2,16 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader } from "@/components/ui/dialog";
-import { DialogTitle } from "@radix-ui/react-dialog";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+
+import FileUpload from "@/components/file-upload";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -61,7 +64,21 @@ const InitialModal = () => {
             className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
