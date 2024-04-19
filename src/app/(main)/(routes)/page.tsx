576dea0a7abba2d99885a0c9ccb8a704693242af
AA <talentyouth99@gmail.com>
2024-04-19 10:48:38 +0800

    Authentication

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,9 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p>
-        This is a protectd route.
-      </p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
