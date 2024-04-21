878e4cb63ff3fd63e049e28ebdbfc418b87f3871
AA <talentyouth99@gmail.com>
2024-04-21 13:14:55 +0800

    Manage Members

---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+interface UserAvatorProps {
+  src?: string,
+  className?: string
+};
+
+const UserAvator = ({ src, className }: UserAvatorProps) => {
+  return (
+    <Avatar className={cn(
+      "h-7 w-7 md:h-10 md:w-10",
+      className
+    )}>
+      <AvatarImage src={src} />
+    </Avatar>
+  );
+}
+
+export default UserAvator;
