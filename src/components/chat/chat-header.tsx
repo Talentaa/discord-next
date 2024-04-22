7b6b587e6a39fe364076805cfa1aa043657122db
AA <talentyouth99@gmail.com>
2024-04-22 22:13:42 +0800

    Conversations Setup

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import MobileToggle from "@/components/mobile-toggle";
+import UserAvator from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId: string,
@@ -21,6 +22,12 @@ const ChatHeader = ({
       {type === "channel" && (
         <Hash className="w-5 h-5 
         text-zinc-500 dark:text-zinc-400 mr-2"
+        />
+      )}
+      {type === "conversation" && (
+        <UserAvator
+          src={imageUrl}
+          className="h-8 w-8 md:h-8 md:w-8 mr-2"
         />
       )}
       <p className="font-semibold text-md 
